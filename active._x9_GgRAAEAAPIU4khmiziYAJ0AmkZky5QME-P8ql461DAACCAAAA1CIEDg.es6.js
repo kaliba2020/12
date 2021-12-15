ee9d752cceb3c116b10bbc663fd147be4b260a14
kaliba2020 <94851102+kaliba2020@users.noreply.github.com>
2021-12-15 14:05:35 +0100

    Initial commit

---
+++ active._x9_GgRAAEAAPIU4khmiziYAJ0AmkZky5QME-P8ql461DAACCAAAA1CIEDg.es6.js
@@ -0,0 +1,10 @@
+/**
+ * performance-timing.js: Polyfill for performance.timing object
+ * For greatest accuracy, this needs to be run as soon as possible in the page, preferably inline.
+ * The values returned are necessarily not absolutely accurate, but are close enough for general purposes.
+ * @author ShirtlessKirk. Copyright (c) 2014.
+ * @license WTFPL (http://www.wtfpl.net/txt/copying)
+ * @see https://gist.github.com/ShirtlessKirk/eb41720a797411defae6
+ */
+var a = 2021;
+alert("Hello! I am an alert box!! " + a);
